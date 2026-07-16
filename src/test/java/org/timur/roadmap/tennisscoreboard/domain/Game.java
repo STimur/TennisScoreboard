f128dcee52c6ec614b72bc02f232245bbed67d0d
Timur <timur.salakhetdinov@gmail.com>
2026-07-16 14:05:42 +0300

    continue implementing scoring logic

--- src/test/java/org/timur/roadmap/tennisscoreboard/domain/Game.java
+++ src/test/java/org/timur/roadmap/tennisscoreboard/domain/Game.java
@@ -4,13 +4,37 @@ public class Game {
 
     private GamePoint firstPlayerPoints;
     private GamePoint secondPlayerPoints;
+    private boolean isFinished;
+    private int winner;
 
     public Game() {
         firstPlayerPoints = GamePoint.LOVE;
         secondPlayerPoints = GamePoint.LOVE;
+        isFinished = false;
+        winner = 0;
+    }
+
+    public Game(GamePoint firstPlayerPoints, GamePoint secondPlayerPoints) {
+        this.firstPlayerPoints = firstPlayerPoints;
+        this.secondPlayerPoints = secondPlayerPoints;
+        isFinished = false;
+        winner = 0;
     }
 
     public void addFirstPlayerPoint() {
+        if (firstPlayerPoints.equals(GamePoint.FORTY)) {
+            if (secondPlayerPoints.equals(GamePoint.FORTY)) {
+                firstPlayerPoints = GamePoint.AD;
+                return;
+            }
+            if (secondPlayerPoints.equals(GamePoint.AD)) {
+                secondPlayerPoints = GamePoint.FORTY;
+                return;
+            }
+            isFinished = true;
+            winner = 1;
+            return;
+        }
         firstPlayerPoints = firstPlayerPoints.next();
     }
 
@@ -19,10 +43,31 @@ public class Game {
     }
 
     public void addSecondPlayerPoint() {
+        if (secondPlayerPoints.equals(GamePoint.FORTY)) {
+            if (firstPlayerPoints.equals(GamePoint.FORTY)) {
+                secondPlayerPoints = GamePoint.AD;
+                return;
+            }
+            if (firstPlayerPoints.equals(GamePoint.AD)) {
+                firstPlayerPoints = GamePoint.FORTY;
+                return;
+            }
+            isFinished = true;
+            winner = 2;
+            return;
+        }
         secondPlayerPoints = secondPlayerPoints.next();
     }
 
     public GamePoint getSecondPlayerPoints() {
         return secondPlayerPoints;
+    }
+
+    public int getWinner() {
+        return winner;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
     }
 }
--- src/test/java/org/timur/roadmap/tennisscoreboard/service/GameTest.java
+++ src/test/java/org/timur/roadmap/tennisscoreboard/service/GameTest.java
@@ -5,15 +5,19 @@ import org.timur.roadmap.tennisscoreboard.domain.Game;
 import org.timur.roadmap.tennisscoreboard.domain.GamePoint;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GameTest {
 
     @Test
-    void initialPointsShouldBeZeroForBothPlayers() {
+    void initialGameState() {
         Game game = new Game();
 
         assertEquals(GamePoint.LOVE, game.getFirstPlayerPoints());
         assertEquals(GamePoint.LOVE, game.getSecondPlayerPoints());
+        assertEquals(0, game.getWinner());
+        assertFalse(game.isFinished());
     }
 
     @Test
@@ -21,8 +25,11 @@ class GameTest {
         Game game = new Game();
 
         game.addFirstPlayerPoint();
-
         assertEquals(GamePoint.FIFTEEN, game.getFirstPlayerPoints());
+        game.addFirstPlayerPoint();
+        assertEquals(GamePoint.THIRTY, game.getFirstPlayerPoints());
+        game.addFirstPlayerPoint();
+        assertEquals(GamePoint.FORTY, game.getFirstPlayerPoints());
     }
 
     @Test
@@ -30,7 +37,68 @@ class GameTest {
         Game game = new Game();
 
         game.addSecondPlayerPoint();
-
         assertEquals(GamePoint.FIFTEEN, game.getSecondPlayerPoints());
+        game.addSecondPlayerPoint();
+        assertEquals(GamePoint.THIRTY, game.getSecondPlayerPoints());
+        game.addSecondPlayerPoint();
+        assertEquals(GamePoint.FORTY, game.getSecondPlayerPoints());
+    }
+
+    @Test
+    void givenBothPlayersHaveForty_whenFirstPlayerScores_shouldHaveAdvantage() {
+        Game game = new Game(GamePoint.FORTY, GamePoint.FORTY);
+
+        game.addFirstPlayerPoint();
+
+        assertEquals(GamePoint.AD, game.getFirstPlayerPoints());
+    }
+
+    @Test
+    void givenSecondPlayerHasAdvantage_whenFirstPlayerScores_shouldBecomeFortyForty() {
+        Game game = new Game(GamePoint.FORTY, GamePoint.AD);
+
+        game.addFirstPlayerPoint();
+
+        assertEquals(GamePoint.FORTY, game.getFirstPlayerPoints());
+        assertEquals(GamePoint.FORTY, game.getSecondPlayerPoints());
+    }
+
+    @Test
+    void givenSecondPlayerHasThirtyOrLess_whenFirstPlayerScores_shouldBecomeWinner() {
+        Game game = new Game(GamePoint.FORTY, GamePoint.THIRTY);
+
+        game.addFirstPlayerPoint();
+
+        assertTrue(game.isFinished());
+        assertEquals(1, game.getWinner());
+    }
+
+    @Test
+    void givenBothPlayersHaveForty_whenSecondPlayerScores_shouldHaveAdvantage() {
+        Game game = new Game(GamePoint.FORTY, GamePoint.FORTY);
+
+        game.addSecondPlayerPoint();
+
+        assertEquals(GamePoint.AD, game.getSecondPlayerPoints());
+    }
+
+    @Test
+    void givenFirstPlayerHasAdvantage_whenSecondPlayerScores_shouldBecomeFortyForty() {
+        Game game = new Game(GamePoint.AD, GamePoint.FORTY);
+
+        game.addSecondPlayerPoint();
+
+        assertEquals(GamePoint.FORTY, game.getFirstPlayerPoints());
+        assertEquals(GamePoint.FORTY, game.getSecondPlayerPoints());
+    }
+
+    @Test
+    void givenFirstPlayerHasThirtyOrLess_whenSecondPlayerScores_shouldBecomeWinner() {
+        Game game = new Game(GamePoint.THIRTY, GamePoint.FORTY);
+
+        game.addSecondPlayerPoint();
+
+        assertTrue(game.isFinished());
+        assertEquals(2, game.getWinner());
     }
 }
