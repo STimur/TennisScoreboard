ebab4b7546b41577402ca0a9389b091ebe006b2e
Timur <timur.salakhetdinov@gmail.com>
2026-09-23 17:17:46 +0300

    frontend added

--- src/main/java/org/timur/roadmap/tennisscoreboard/config/WebConfig.java
+++ src/main/java/org/timur/roadmap/tennisscoreboard/config/WebConfig.java
@@ -3,6 +3,8 @@ package org.timur.roadmap.tennisscoreboard.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,4 +12,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan(basePackages = "org.timur.roadmap.tennisscoreboard")
 public class WebConfig implements WebMvcConfigurer {
     // You can override methods here to add View Resolvers or Resource Handlers
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Разрешаем доступ к любым файлам в корне webapp
+        registry.addResourceHandler("/**")
+                .addResourceLocations("/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // При переходе на "/" (корень приложения) автоматически отдавать index.html
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
 }
